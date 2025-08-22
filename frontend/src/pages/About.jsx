c049110d5408662b02c7c85b41e75c0afea264d6
Bigmanbiggiey <ndiranguh02@gmail.com>
2025-08-22 21:13:48 +0300

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
     <section className="p-8 max-w-4xl mx-auto">
       <h2 className="text-3xl font-bold text-teal-700 mb-4">About Me</h2>
       <p className="text-gray-700">
-        Hi! I’m a passionate pet lover based in Kitengela. I’ve been dog sitting and minding pets for over 5 years...
+        Hi! I’m a passionate pet lover based in Nairobi. I’ve been dog sitting and minding pets for over 5 years...
       </p>
     </section>
   );
